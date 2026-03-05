e1726a5b72f71caab461e0966f810d1a5728610d
Sangmin Kang <kkang5430@naver.com>
2026-03-05 14:37:27 +0900

    init

---
+++ src/main/java/com/fc/fcseoularchive/FcseoulArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.fc.fcseoularchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcseoulArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FcseoulArchiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fc/fcseoularchive/FcseoulArchiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fc.fcseoularchive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FcseoulArchiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
