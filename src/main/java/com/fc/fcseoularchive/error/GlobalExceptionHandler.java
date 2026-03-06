bf7c52680fb618b99dd180758ce8a697031e2467
inwoohub <dlsdndls@naver.com>
2026-03-06 17:49:32 +0900

    #3 test: 테스트

--- src/main/java/com/fc/fcseoularchive/error/GlobalExceptionHandler.java
+++ src/main/java/com/fc/fcseoularchive/error/GlobalExceptionHandler.java
@@ -11,15 +11,10 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
  * 전역 예외처리
  * 사용법 :
  * throw new ApiException(HttpStatus.NOT_FOUND, "404", "NOT_FOUND", "유저를 찾을 수 없습니다");
- * <p>
- * 출력 예시 :
- * HTTP/1.1 404 Not Found
- * Content-Type: application/json
- * {
- * "status" : "404",
- * "code": "USER_NOT_FOUND",
- * "message": "유저를 찾을 수 없습니다."
- * }
+ *
+ *  HttpServletRequest?
+ *  현재 들어온 HTTP 요청 자체를 담고있는 객체임
+ *  요청 URL, HTTP method, 헤더, 쿼리 파라미터, 클라이언트 정보 조회 가능
  */
 
 @RestControllerAdvice
