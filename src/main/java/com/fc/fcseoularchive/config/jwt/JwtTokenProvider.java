035e7bcfec152151e283a231ae196ec0d3613259
inwoohub <dlsdndls@naver.com>
2026-03-07 14:18:54 +0900

    #3 feat: jwt 생성 & 검증 등등 provider 구현

--- src/main/java/com/fc/fcseoularchive/config/jwt/JwtTokenProvider.java
+++ src/main/java/com/fc/fcseoularchive/config/jwt/JwtTokenProvider.java
@@ -14,10 +14,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 
-import java.security.Key; // 암호화 용도
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Arrays;
-import java.util.Base64;
 import java.util.Collection;
 import java.util.Date;
 import java.util.stream.Collectors;
@@ -31,21 +31,21 @@ import java.util.stream.Collectors;
 @Component
 public class JwtTokenProvider {
 
-    private final Key key;
+    private final SecretKey key;
     private final UserDetailsService userDetailsService; // 스프링 시큐리티에서 제공하는 사용자 조회용 인터페이스임
     private final RedisDao redisDao;
 
     private static final String GRANT_TYPE = "Bearer";
 
-    @Value("${jwt.access-token.expire-time}")
+    @Value("${jwt.access-token.expire-time}") // 1시간으로 설정
     private long ACCESS_TOKEN_EXPIRE_TIME;
 
-    @Value("${jwt.refresh-token.expire-time}")
+    @Value("${jwt.refresh-token.expire-time}") // 7일으로 설정
     private long REFRESH_TOKEN_EXPIRE_TIME;
 
     public JwtTokenProvider(@Value("${jwt.secret}") String secretKey, UserDetailsService userDetailsService, RedisDao redisDao) {
 
-        byte[] keyBytes = secretKey.getBytes();
+        byte[] keyBytes = secretKey.getBytes(StandardCharsets.UTF_8);
         this.key = Keys.hmacShaKeyFor(keyBytes);
         this.userDetailsService = userDetailsService;
         this.redisDao = redisDao;
@@ -69,7 +69,7 @@ public class JwtTokenProvider {
 
         // AccessToken 생성하기
         Date accessTokenExpire = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
-        String accessToken = generateAccessToken(username, authorities, userRole, accessTokenExpire);
+        String accessToken = generateAccessToken(username, userRole, accessTokenExpire);
 
 
         // RefreshToken 생성하기
@@ -86,18 +86,16 @@ public class JwtTokenProvider {
 
 
     // 토큰 생성자
-    private String generateAccessToken(String username, String authorities, String userRole, Date expireDate) {
+    public String generateAccessToken(String username, String userRole, Date expireDate) {
         return Jwts.builder()
-                .setSubject(username) // 토큰 제목 (username)
-                .claim("auth", authorities) // 권한 정보 (커스텀 클레임)
-                .claim("userRole", userRole)
+                .setSubject(username) // 토큰 제목 (username) < Subject에
+                .claim("role", userRole) // userRole 넣기 ("ADMIN", "USER") 넣을 예정
                 .setExpiration(expireDate) // 토큰 만료 시간
                 .signWith(key, SignatureAlgorithm.HS256) // 지정돤 키와, 알고리즘 으로 서명
                 .compact(); // 최종 JWT 문자열 생성 (header.payload.signature 구조임!)
-
     }
 
-
+    // 리프레시 토큰 생성자
     private String generateRefreshToken(String username, Date expireDate) {
         return Jwts.builder()
                 .setSubject(username)
@@ -111,19 +109,19 @@ public class JwtTokenProvider {
     public Authentication getAuthentication(String accessToken) {
         // JWT 토큰 복호화
         Claims claims = parseClaims(accessToken);
-        if (claims.get("auth") == null) {
+        if (claims.get("role") == null) {
             throw new RuntimeException("권한 정보가 없는 토큰입니다.");
         }
 
         // 클레임에서 권한 정보 가져오기
-        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get("auth").toString().split(","))
+        Collection<? extends GrantedAuthority> authorities = Arrays.stream(claims.get("role").toString().split(","))
                 .map(SimpleGrantedAuthority::new) // SimpleGrantedAuthority 객체들의 컬렉션으로 변환
                 .toList();
 
         // UserDetails 객체를 만들어서 Authentication return
         // UserDetails: interface, User: UserDetails를 구현한 클래스
         UserDetails principal = new User(claims.getSubject(), "", authorities); // 파라미터: 사용자 식별자, credentials, 권한 목록
-        return new UsernamePasswordAuthenticationToken(principal, "", authorities);
+        return new UsernamePasswordAuthenticationToken(principal, null, authorities);
 
     }
 
@@ -131,10 +129,11 @@ public class JwtTokenProvider {
     private Claims parseClaims(String accessToken) {
         try {
             return Jwts.parser()
-                    .setSigningKey(key)
+                    .verifyWith(key)
                     .build()
-                    .parseClaimsJws(accessToken) // JWT 토큰 검증과 파싱을 모두 수행함
-                    .getBody();
+                    .parseSignedClaims(accessToken)
+                    .getPayload();
+
         } catch (ExpiredJwtException e) {
             return e.getClaims();
         }
@@ -144,9 +143,9 @@ public class JwtTokenProvider {
     public boolean validateToken(String token) {
         try {
             Jwts.parser()
-                    .setSigningKey(key)
+                    .verifyWith(key)
                     .build()
-                    .parseClaimsJws(token);
+                    .parseSignedClaims(token);
 
             return true;
         } catch (SecurityException | MalformedJwtException e) {
@@ -160,7 +159,6 @@ public class JwtTokenProvider {
         }
         return false;
     }
-
 
 
     // RefreshToken 검증
@@ -185,12 +183,12 @@ public class JwtTokenProvider {
         try {
             // 토큰 파싱해서 클레임 얻기
             Claims claims = Jwts.parser()
-                    .setSigningKey(key)
+                    .verifyWith(key)
                     .build()
-                    .parseClaimsJws(token)
-                    .getBody();
+                    .parseSignedClaims(token)
+                    .getPayload();
 
-            // 사용자 이름(subject) 반환
+            // 사용자 이름(subject) 반환 -> subject에 이름을 넣어뒀었음
             return claims.getSubject();
         } catch (ExpiredJwtException e) {
             // 토큰이 만료되어도 클레임 내용을 가져올 수 있음
@@ -209,38 +207,37 @@ public class JwtTokenProvider {
     }
 
 
-
 }
 
 /**
  * jwt 는 크게 Header.payload.Signature 3 단계다.
- *
+ * <p>
  * 1: Header
  * -> 토큰의 메타 정보다 {"alg" : "HS256" , "typ":"JWT"} 처럼!
- *
+ * <p>
  * 2: payload
  * -> 토큰 내부 정보다!
- *  .setSubject(username)은 누구껀지!
- *      * 그렇게 하면 payload 안에서 보통 sub 라는 이름으로 들어간다.
- *      * ex ) { "sub" : "username" } 처럼
- *
- *  . claim(..) 이거는 쉽게 생각하면 커스텀이다.
- *      * 필요한 정보들을 여기서 추가하면 된다.
- *      * 그러면 또
- *      * ex) { "auth" : "authorities", "userRole" : "userRole" } 처럼 payload에 들어간다.
- *
- *3: Signature
+ * .setSubject(username)은 누구껀지!
+ * * 그렇게 하면 payload 안에서 보통 sub 라는 이름으로 들어간다.
+ * * ex ) { "sub" : "username" } 처럼
+ * <p>
+ * . claim(..) 이거는 쉽게 생각하면 커스텀이다.
+ * * 필요한 정보들을 여기서 추가하면 된다.
+ * * 그러면 또
+ * * ex) { "auth" : "authorities", "userRole" : "userRole" } 처럼 payload에 들어간다.
+ * <p>
+ * 3: Signature
  * -> 위조 방지 서명이다.
- *  HMACSHA256{ base64UrlEncode(header) + "." + bas64UrlEncode(payload
- *              , secretKey} 처럼 되었이다.
- *   즉, 서버는 같은 secret key로 다시 계산 해보고 서명이 같으면, 정상, 다르면 위조로 판단한다.
- *
+ * HMACSHA256{ base64UrlEncode(header) + "." + bas64UrlEncode(payload
+ * , secretKey} 처럼 되었이다.
+ * 즉, 서버는 같은 secret key로 다시 계산 해보고 서명이 같으면, 정상, 다르면 위조로 판단한다.
+ * <p>
  * 그럼? Header 가 있는데 Signature 가 왜 필요하나..?
  * payload 는 그냥 인코딩한 Signature를 통해서 진짜인지 아닌지 확인하는 것이다.
- *
+ * <p>
  * 정리하자면, Header는 이 토큰이 어떤 방식으로 만들어 졌는지 알려주고,
  * 시그니처는 이 토큰이 중간에 안 바뀌고, 진짜 서버가 만든게 맞는지 검증하는 장치다.
- *
+ * <p>
  * 1. Header JSON 생성
  * 2. Payload JSON 생성
  * 3. Header를 base64Url 인코딩
@@ -248,7 +245,7 @@ public class JwtTokenProvider {
  * 5. 인코딩된 Header + "." + 인코딩된 Payload 를 만든다
  * 6. 그 값을 secretKey 와 알고리즘(예: HS256)으로 서명해서 Signature를 만든다
  * 7. 최종적으로
- *    인코딩된 Header + "." + 인코딩된 Payload + "." + 인코딩된 Signature
- *    를 붙인 것이 JWT 토큰이다. 근데 이건 내가 하는게 아니라 Jwts 라이브러리에서 해주고 String 으로 반환해주기 때문에
- *    나는 가져다만 쓴다.
+ * 인코딩된 Header + "." + 인코딩된 Payload + "." + 인코딩된 Signature
+ * 를 붙인 것이 JWT 토큰이다. 근데 이건 내가 하는게 아니라 Jwts 라이브러리에서 해주고 String 으로 반환해주기 때문에
+ * 나는 가져다만 쓴다.
  */
--- src/test/java/com/fc/fcseoularchive/inwooTest/jwtTest.java
+++ src/test/java/com/fc/fcseoularchive/inwooTest/jwtTest.java
@@ -1,19 +1,153 @@
 package com.fc.fcseoularchive.inwooTest;
 
+import com.fc.fcseoularchive.config.jwt.JwtToken;
+import com.fc.fcseoularchive.config.jwt.JwtTokenProvider;
+import com.fc.fcseoularchive.config.redis.RedisDao;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.security.Principal;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
+
+/**
+ * SecurityContext
+ * └─ Authentication
+ * ├─ principal -> UserDetail 이 들어감
+ * [Username=inwoo, Password=[PROTECTED],
+ * Enabled=true, AccountNonExpired=true,
+ * CredentialsNonExpired=true, AccountNonLocked=true, Granted Authorities=[ADMIN]]
+ * ├─ credentials
+ * └─ authorities
+ * 처럼 구조되어있음
+ * <p>
+ * 시큐리티 컨텍스트는 로컬스레드로 관리되며
+ * 하나의 동작이 끝나면 clear 후 스레드 반환하는 방식으로 동작함
+ * <p>
+ * Authenticatin 을 만들기 위해 생성하랴고 보니
+ * principal, credential,  + authorities (이건 추가 넣고싶은거 ex: ROLE_USER, ROLE_ADMIN 등)
+ * principal : 누구? (id)
+ * credentials : 뭐로 인증? (비밀번호)
+ * authorities : 뭐 할수있음? (동작
+ */
 
 @SpringBootTest
 public class jwtTest {
 
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    // 가짜 레디스
+    @MockBean
+    private RedisDao redisDao;
+
     @Test
     @DisplayName("uuid 하나 아무꺼나 쓰기 jwt secret에 넣을 것")
-    public void test1(){
+    public void test1() {
         UUID uuid = UUID.randomUUID();
         System.out.println(uuid.toString());
     }
+
+    @Test
+    @DisplayName("토큰 생성 테스트")
+    public void test2() {
+
+        // 토큰 생성
+        Date date = new Date(System.currentTimeMillis() + 36000);
+        String accessToken = jwtTokenProvider.generateAccessToken("inwoo", "ADMIN", date);
+        System.out.println(accessToken);
+    }
+
+
+    @Test
+    @DisplayName("토큰 Access, Refresh 생성 테스트")
+    void test3() {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                "inwoo",
+                null,
+                List.of(new SimpleGrantedAuthority("ADMIN"))
+        );
+
+        JwtToken jwtToken = jwtTokenProvider.generateToken(authentication);
+
+        System.out.println(jwtToken.getAccessToken());
+        System.out.println(jwtToken.getRefreshToken());
+
+        String userNameFromToken = jwtTokenProvider.getUserNameFromToken(jwtToken.getAccessToken());
+        System.out.println(userNameFromToken);
+
+    }
+
+    @Test
+    @DisplayName("토큰 복호화")
+    public void test4() {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                "inwoo",
+                null,
+                List.of(new SimpleGrantedAuthority("ADMIN"), new SimpleGrantedAuthority("메롱!"))
+        );
+
+        JwtToken jwtToken = jwtTokenProvider.generateToken(authentication);
+
+        String accessToken = jwtToken.getAccessToken();
+
+        Authentication authentication1 = jwtTokenProvider.getAuthentication(accessToken); // 복호화
+
+        Assertions.assertEquals(
+                "inwoo",
+                ((UserDetails) authentication1.getPrincipal()).getUsername()
+        );
+
+        Collection<? extends GrantedAuthority> authorities = ((UserDetails) authentication1.getPrincipal()).getAuthorities();
+        for (GrantedAuthority authority : authorities) {
+            System.out.println("authority = " + authority);
+        }
+
+
+    }
+
+    @Test
+    @DisplayName("리프레시 토큰 삭제")
+    public void test5() {
+        Authentication authentication = new UsernamePasswordAuthenticationToken(
+                "inwoo",
+                null,
+                List.of(new SimpleGrantedAuthority("ADMIN"))
+        );
+        JwtToken jwtToken = jwtTokenProvider.generateToken(authentication);
+
+        jwtTokenProvider.deleteRefreshToken(jwtTokenProvider.getUserNameFromToken(jwtToken.getRefreshToken())); // 토큰 삭제
+
+        jwtTokenProvider.validateRefreshToken(jwtTokenProvider.getUserNameFromToken(jwtToken.getRefreshToken())); // 오류 예상?
+
+    }
+
+
+    @Test
+    @DisplayName("메롱 토큰 확인")
+    public void test6() throws Exception {
+        Date date = new Date(System.currentTimeMillis() + 36000);
+
+//        String s = jwtTokenProvider.generateAccessTokenTest("inwoo", "메롱", "ADMIN", date);
+
+//        System.out.println("s = " + s);
+
+        // 토큰 복호화
+//        Authentication authentication = jwtTokenProvider.getAuthenticationTest(s);
+//        System.out.println(authentication.getPrincipal());
+
+    }
+
 
 }
