f9a96ec8d938e58ccd9eae0634b0980503ccd771
inwoohub <dlsdndls@naver.com>
2026-03-15 17:07:37 +0900

    #22 feat: 1일 1회 출석체크 & 연속 출석 일수 제공

--- src/main/java/com/fc/fcseoularchive/domain/entity/User.java
+++ src/main/java/com/fc/fcseoularchive/domain/entity/User.java
@@ -1,6 +1,5 @@
 package com.fc.fcseoularchive.domain.entity;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fc.fcseoularchive.domain.enums.Role;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -40,6 +39,9 @@ public class User {
     @Column(name = "profile_image", length = 512)
     private String profileImage;
 
+    @Column(name = "attendance_streak")
+    private Integer attendanceStreak;
+
     @Column(name = "last_login")
     private LocalDateTime lastLogin;
 
@@ -59,6 +61,7 @@ public class User {
         this.nickname = nickname;
         this.role = Role.USER;
         this.points = 0;
+        this.attendanceStreak = 0;
         this.profileImage = null;
         this.lastLogin = null;
         this.deletedAt = null;
@@ -95,10 +98,6 @@ public class User {
         this.profileImage = profileImage;
     }
 
-    public void updateLastLogin(LocalDateTime lastLogin) {
-        this.lastLogin = lastLogin;
-    }
-
     public void changeRole(Role role) {
         this.role = role;
     }
@@ -109,6 +108,18 @@ public class User {
 
     public void softDelete() {
         this.deletedAt = LocalDateTime.now();
+    }
+
+    public void lastLoginUpdate(){
+        this.lastLogin = LocalDateTime.now();
+    }
+
+    public void addAttendanceStreak() {
+        this.attendanceStreak += 1 ;
+    }
+
+    public void initAttendanceStreak() {
+        this.attendanceStreak = 1;
     }
 
 }
